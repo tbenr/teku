d878b2a298c56d6e2ac8ab22b27b20fa8002b9db
Enrico Del Fante <enrico.delfante@consensys.net>
2023-07-13 17:13:44 +0200

    benches
    benches

--- eth-benchmark-tests/src/jmh/java/tech/pegasys/teku/benchmarks/SSZBenchmark.java
+++ eth-benchmark-tests/src/jmh/java/tech/pegasys/teku/benchmarks/SSZBenchmark.java
@@ -13,19 +13,74 @@
 
 package tech.pegasys.teku.benchmarks;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.concurrent.TimeUnit;
+
+import com.google.common.io.ByteStreams;
+import org.apache.tuweni.bytes.Bytes;
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.infra.Blackhole;
+import tech.pegasys.teku.benchmarks.util.CustomRunner;
 import tech.pegasys.teku.infrastructure.ssz.SimpleOffsetSerializable;
+import tech.pegasys.teku.spec.Spec;
 import tech.pegasys.teku.spec.TestSpecFactory;
 import tech.pegasys.teku.spec.datastructures.execution.ExecutionPayload;
+import tech.pegasys.teku.spec.datastructures.state.beaconstate.BeaconState;
+import tech.pegasys.teku.spec.datastructures.state.beaconstate.BeaconStateSchema;
 import tech.pegasys.teku.spec.util.DataStructureUtil;
 
 public class SSZBenchmark {
   private static DataStructureUtil dataStructureUtil =
       new DataStructureUtil(TestSpecFactory.createMinimalBellatrix());
   private static SimpleOffsetSerializable state = dataStructureUtil.randomBeaconState();
+
+  private static Spec capellaSpec = TestSpecFactory.createMainnetCapella();
+private static BeaconStateSchema<?, ?> beaconStateSchema = capellaSpec.getGenesisSchemaDefinitions().getBeaconStateSchema();
+
+  private static Path inputPath = Path.of("finalizedstate.ssz");
+  private static InputStream inputStream;
+  private static Bytes inData;
+  private static BeaconState deserializedBeaconState;
+
+  static {
+    try {
+      inputStream = Files.newInputStream(inputPath);
+      inData = Bytes.wrap(ByteStreams.toByteArray(inputStream));
+      deserializedBeaconState = beaconStateSchema.sszDeserialize(inData);
+      deserializedBeaconState.getBalances();
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Benchmark @BenchmarkMode(Mode.AverageTime) @OutputTimeUnit(TimeUnit.MILLISECONDS)
+  @Warmup(iterations = 2, time = 100, timeUnit = TimeUnit.MILLISECONDS)
+  @Measurement(iterations = 5, time = 100, timeUnit = TimeUnit.MILLISECONDS)
+  public void BeaconStateDeserialization() {
+    beaconStateSchema.sszDeserialize(inData);
+  }
+
+  @Benchmark @BenchmarkMode(Mode.AverageTime) @OutputTimeUnit(TimeUnit.MILLISECONDS)
+  @Warmup(iterations = 0, time = 100, timeUnit = TimeUnit.MILLISECONDS)
+  @Measurement(iterations = 1, time = 10, timeUnit = TimeUnit.MILLISECONDS)
+  public void BeaconStateGetBalances() {
+    deserializedBeaconState.getBalances().get(1);
+  }
+
+  @Benchmark @BenchmarkMode(Mode.AverageTime) @OutputTimeUnit(TimeUnit.MILLISECONDS)
+  @Warmup(iterations = 0, time = 100, timeUnit = TimeUnit.MILLISECONDS)
+  @Measurement(iterations = 1, time = 10, timeUnit = TimeUnit.MILLISECONDS)
+  public void BeaconStateHashing(Blackhole bh) {
+    bh.consume(deserializedBeaconState.hashTreeRoot());
+  }
 
   @Benchmark
   @Warmup(iterations = 2, time = 100, timeUnit = TimeUnit.MILLISECONDS)
@@ -41,5 +96,11 @@ public class SSZBenchmark {
   @Measurement(iterations = 5, time = 100, timeUnit = TimeUnit.MILLISECONDS)
   public void ExecutionPayloadIsDefault() {
     executionPayload.isDefault();
+  }
+
+  public static void main(String[] args) {
+    SSZBenchmark asd = new SSZBenchmark();
+
+    new CustomRunner(1,1).withBench(asd::BeaconStateHashing).run();
   }
 }
