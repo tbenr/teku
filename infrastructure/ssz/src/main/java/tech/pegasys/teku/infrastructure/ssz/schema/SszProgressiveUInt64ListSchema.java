e998ef6b7c4c37fd87cce9df34e7baf2f1c89363
Enrico Del Fante <enrico.delfante@consensys.net>
2026-02-25 13:09:23 +0100

    add SszProgressiveUInt64list classes

---
+++ infrastructure/ssz/src/main/java/tech/pegasys/teku/infrastructure/ssz/schema/SszProgressiveUInt64ListSchema.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright Consensys Software Inc., 2026
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package tech.pegasys.teku.infrastructure.ssz.schema;
+
+import tech.pegasys.teku.infrastructure.ssz.SszList;
+import tech.pegasys.teku.infrastructure.ssz.collections.impl.SszProgressiveUInt64ListImpl;
+import tech.pegasys.teku.infrastructure.ssz.primitive.SszUInt64;
+import tech.pegasys.teku.infrastructure.ssz.tree.TreeNode;
+
+/**
+ * Schema for a progressive list of UInt64 values. Extends {@link SszProgressiveListSchema} with
+ * hardcoded UINT64 element schema and overrides {@link #createFromBackingNode} to produce {@link
+ * SszProgressiveUInt64ListImpl} instances that implement {@link
+ * tech.pegasys.teku.infrastructure.ssz.collections.SszUInt64List}.
+ */
+public class SszProgressiveUInt64ListSchema extends SszProgressiveListSchema<SszUInt64> {
+
+  private SszProgressiveUInt64ListSchema(final SszSchemaHints hints) {
+    super(SszPrimitiveSchemas.UINT64_SCHEMA, hints);
+  }
+
+  public static SszProgressiveUInt64ListSchema create() {
+    return new SszProgressiveUInt64ListSchema(SszSchemaHints.none());
+  }
+
+  public static SszProgressiveUInt64ListSchema create(final SszSchemaHints hints) {
+    return new SszProgressiveUInt64ListSchema(hints);
+  }
+
+  @Override
+  public SszList<SszUInt64> createFromBackingNode(final TreeNode node) {
+    return new SszProgressiveUInt64ListImpl(this, node);
+  }
+}
