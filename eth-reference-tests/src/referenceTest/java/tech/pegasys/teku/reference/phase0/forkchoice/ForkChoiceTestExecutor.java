5ab6b5c919f0afee6fc4c671fc63500c279ccaf0
Enrico Del Fante <enrico.delfante@consensys.net>
2026-03-23 17:52:24 +0100

    unskip a forkchoice reference test

--- eth-reference-tests/src/referenceTest/java/tech/pegasys/teku/reference/phase0/forkchoice/ForkChoiceTestExecutor.java
+++ eth-reference-tests/src/referenceTest/java/tech/pegasys/teku/reference/phase0/forkchoice/ForkChoiceTestExecutor.java
@@ -115,7 +115,7 @@ public class ForkChoiceTestExecutor implements TestExecutor {
           .put("fork_choice/withholding", new ForkChoiceTestExecutor())
           .put("sync/optimistic", new ForkChoiceTestExecutor())
           .put("fork_choice/should_override_forkchoice_update", new ForkChoiceTestExecutor())
-          .put("fork_choice/get_proposer_head", new ForkChoiceTestExecutor("basic_is_parent_root"))
+          .put("fork_choice/get_proposer_head", new ForkChoiceTestExecutor())
           .put("fork_choice/deposit_with_reorg", new ForkChoiceTestExecutor())
           .put("fork_choice/base", new ForkChoiceTestExecutor())
           // Fork choice generated test types
